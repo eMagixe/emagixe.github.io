b7e91b6ddced38f07cb1608c517daffd471c5ce7
max evchenko <emax.mails@gmail.com>
2019-11-13 20:39:49 +0500

    Init react-todo-demo

---
+++ react-todo-demo/precache-manifest.cc04d12991c598fce72607f10fe54dab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "333b5b24d677abbd92c37c29a32117d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03126d86b7e89372f850",
+    "url": "/static/css/main.cfb36ed5.chunk.css"
+  },
+  {
+    "revision": "1026e663629320b841f0",
+    "url": "/static/js/2.6c964e58.chunk.js"
+  },
+  {
+    "revision": "03126d86b7e89372f850",
+    "url": "/static/js/main.7264a5e5.chunk.js"
+  },
+  {
+    "revision": "434872893da42ccf6f6f",
+    "url": "/static/js/runtime-main.9f9cf6c2.js"
+  }
+]);
